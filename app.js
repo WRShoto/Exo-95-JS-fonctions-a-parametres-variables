e7cc2fcdc005c09e8369a6bb1b2804f2388187bf
benji <Benji59btw@gmail.com>
2022-12-08 16:41:13 +0100

    pas fini

--- app.js
+++ app.js
@@ -6,8 +6,18 @@ function paramVAR(...bonjour){
     for (let name of bonjour){
         P.innerHTML += name + ", ";
     }
-
     P.innerHTML = P.innerHTML.trim().slice(0, -1);
+    if (a<5){
+        paramVAR('param1','param2','param3','param4')
+    }
+
 }
 paramVAR(' param1','param2','param3')
 
+
+//2
+
+
+function number(un,deux,trois){
+
+}
